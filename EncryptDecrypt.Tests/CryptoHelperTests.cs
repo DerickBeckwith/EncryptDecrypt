2cba2b4eca76fec0555e91d9b671bdc2cc781286
Derick Beckwith <derick.beckwith@gmail.com>
2018-06-01 17:56:43 -0400

    Add a couple of unit tests

--- EncryptDecrypt.Tests/CryptoHelperTests.cs
+++ EncryptDecrypt.Tests/CryptoHelperTests.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
 
 namespace EncryptDecrypt.Tests
 {
+    [TestFixture]
     public class CryptoHelperTests
     {
+        private const string Secret = "Secret Message to Encrypt";
+        private const string Password = "SuperSecretPassword";
+
+        [Test]
+        public void DecryptReturnsNonNullString()
+        {
+            // Act
+            var cipher = CryptoHelper.EncryptWithPassword(Secret, Password);
+
+            // Assert
+            Assert.IsNotNull(cipher);
+        }
+
+        [Test]
+        public void DecryptReturnsNonEmptyString()
+        {
+            // Act
+            var cipher = CryptoHelper.EncryptWithPassword(Secret, Password);
+
+            // Assert
+            Assert.IsNotEmpty(cipher);
+        }
     }
 }
