385c3747fb5bdd04a47831a8fad9a30923ec602d
Derick Beckwith <derick.beckwith@gmail.com>
2018-06-01 12:36:42 -0400

    Add code to test the CryptoHelper class

--- EncryptDecrypt/Program.cs
+++ EncryptDecrypt/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
 
 namespace EncryptDecrypt
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static readonly string Password = ConfigurationManager.AppSettings["CryptoKey"];
+
+        private static void Main(string[] args)
         {
+            var secret = "The secret message";
+            if (args.Length > 0 && !string.IsNullOrWhiteSpace(args[0])) secret = args[0];
+            Console.WriteLine("Secret message to encrypt: {0}", secret);
+            Console.WriteLine();
+
+            Console.WriteLine("Password used to encrypt/decrypt secret message: {0}", Password);
+            Console.WriteLine();
+
+            var cipher = CryptoHelper.EncryptWithPassword(secret, Password);
+            Console.WriteLine("Encrypted cipher text: {0}", cipher);
+            Console.WriteLine();
+
+            var plain = CryptoHelper.DecryptWithPassword(cipher, Password);
+            Console.WriteLine("Decrypted plain text: {0}", plain);
+            Console.ReadLine();
         }
     }
 }
